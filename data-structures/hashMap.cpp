f8c7a6384216768553ec4fa7d07929bf5d95da15
Kishore. V <157293196+vk22006@users.noreply.github.com>
2025-08-18 09:58:30 +0530

    Create hashMap.cpp

---
+++ data-structures/hashMap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "hash.h"
+
+int main() {
+    HashMap map;
+    map.insert(17, "ABC");
+    map.insert(23, "DEF");
+    map.insert(36, "GHI");
+    map.insert(19, "JKL");
+    map.insert(37, "KLM");
+
+    map.display();
+
+    std::cout << "Value at 36: " << map.get(36);
+
+    map.deleteval(19);
+    map.display();
+
+    return 0;
+}
